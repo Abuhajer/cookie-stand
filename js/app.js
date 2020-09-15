e27de8b85ff90483365f329386314780dd2b8d2c
Abuhajer <Abuhagerr@gmail.com>
2020-09-15 09:57:08 +0300

    clean the code

--- js/app.js
+++ js/app.js
@@ -1,24 +1,28 @@
 'use strict';
 
-
-var continer = document.getElementById('myList');
-var tableEl = document.createElement('table');
-var totalbyHour=[];
-var totalCustsHours=0;
 function getRandomInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
 }
-function totalPerHour (){
+//---------------
+var continer = document.getElementById('myList');
+var tableEl = document.createElement('table');
+var totalByHour=[];
+var totalCustsHours=0;
+//----------------------------
+//Total of Total Caluclted
+function totalPerHour ()
+{
   for (var i=0 ;i<14;i++)
   {
-    totalbyHour.push(cookiesSeattle.result[i]+cookiesTokyo.result[i]+cookiesDubai.result[i]+cookiesParis.result[i]+cookiesLima.result[i]);
-    totalCustsHours+=totalbyHour[i];
+    totalByHour.push(cookiesSeattle.result[i]+cookiesTokyo.result[i]+cookiesDubai.result[i]+cookiesParis.result[i]+cookiesLima.result[i]);
+    totalCustsHours+=totalByHour[i];
   }
 
 }
-
+//---------------
+//Table Header
 function headerRender (){
 
   continer.appendChild(tableEl);
@@ -41,7 +45,8 @@ function headerRender (){
   thEl.appendChild(thEls);
   thEls.textContent = 'Daily Location Total';
 }
-//-----
+//---------------------------
+//total of total
 function footerRender (){
   totalPerHour();
   var trEl = document.createElement('tr');
@@ -52,7 +57,7 @@ function footerRender (){
   for (var i = 6; i <= 19; i++) {
     tdEls = document.createElement('td');
 
-    tdEls.textContent = `${totalbyHour[i-6]}`;
+    tdEls.textContent = `${totalByHour[i-6]}`;
     trEl.appendChild(tdEls);
 
   }
@@ -60,7 +65,7 @@ function footerRender (){
   trEl.appendChild(tdEls);
   tdEls.textContent=`${totalCustsHours}`;
 }
-
+//Create Object Constructor
 //-----------------------
 function CookiesSection(location, min, max, avgCookies) {
   this.location = location;
@@ -71,10 +76,13 @@ function CookiesSection(location, min, max, avgCookies) {
   this.result = [];
 
 }
+//--------
+//Random Customer Number
 CookiesSection.prototype.randomCustNum = function () {
   return getRandomInt(this.min, this.max);
 };
-
+//--------
+//Make The Table
 CookiesSection.prototype.htmlEdit = function () {
 
   var tdEls = document.createElement('td');
@@ -99,29 +107,33 @@ CookiesSection.prototype.htmlEdit = function () {
   trEl.appendChild(tdEls);
 
 };
-//-------------Create Obects
+//-------------
+//Create Obects
 var cookiesSeattle = new CookiesSection('Seattle', 23, 65, 6.3);
 var cookiesTokyo = new CookiesSection('Tokyo', 3, 24, 1.2);
 var cookiesDubai = new CookiesSection('Dubai', 11, 38, 3.7);
 var cookiesParis = new CookiesSection('Paris', 20, 38, 2.3);
 var cookiesLima = new CookiesSection('Lima', 2, 16, 4.6);
-//------------Header
-
+//------------
+//Table Header call
 headerRender();
-//--------------------------------------------Call Objects ... body
+//--------------------------------------------
+//Call Objects Function ... Genrate Random Customer+Table body
+//#1
 cookiesSeattle.randomCustNum();
 cookiesSeattle.htmlEdit();
-// //-----------
+//-----------#2
 cookiesTokyo.randomCustNum();
 cookiesTokyo.htmlEdit();
-//-----------
+//-----------#3
 cookiesDubai.randomCustNum();
 cookiesDubai.htmlEdit();
-//-----------
+//-----------#4
 cookiesParis.randomCustNum();
 cookiesParis.htmlEdit();
-//-----------
+//-----------#5
 cookiesLima.randomCustNum();
 cookiesLima.htmlEdit();
-//-----------footer
+//-----------
+//Table Footer call
 footerRender();
